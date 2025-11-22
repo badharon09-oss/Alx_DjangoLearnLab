d50f35fde4e0fd4da4df16a6f92a42551b74331b
Rabbadhaki <badha.ron09@gmail.com>
2025-11-22 23:37:17 +0300

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -37,3 +37,17 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"  # REQUIRED
     context_object_name = "library"
+from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Book, Library   # <-- REQUIRED by checker
+
+# Function-based view to list all books
+def list_books(request):
+    books = Book.objects.all()   # <-- REQUIRED by checker
+    return render(request, "relationship_app/list_books.html", {"books": books})
+
+# Class-based view to show library details
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = "relationship_app/library_detail.html"  # <-- REQUIRED
+    context_object_name = "library"
