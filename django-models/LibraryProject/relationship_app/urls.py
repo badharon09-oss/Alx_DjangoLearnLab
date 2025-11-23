69584a029c483707a506d6237a05a53878a286c7
Rabbadhaki <badha.ron09@gmail.com>
2025-11-23 10:24:11 +0300

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,18 +1,16 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views
 
 urlpatterns = [
-    path("books/", list_books, name="list_books"),
-    path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
-]
+    # Function-based view
+    path("books/", views.list_books, name="list_books"),
 
-from django.urls import path
-from .views import list_books, LibraryDetailView, user_login, user_logout, register
+    # Class-based view
+    path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),
 
-urlpatterns = [
-    path("books/", list_books, name="list_books"),
-    path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
-    path("login/", user_login, name="login"),
-    path("logout/", user_logout, name="logout"),
-    path("register/", register, name="register"),
+    # Authentication
+    path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),  # REQUIRED
+    path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),  # REQUIRED
+    path("register/", views.register, name="register"),  # REQUIRED
 ]
