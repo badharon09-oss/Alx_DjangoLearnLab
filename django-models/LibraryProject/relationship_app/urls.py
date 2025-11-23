993ea1bc089bf5d02c4a069c18411602997fa3bc
Rabbadhaki <badha.ron09@gmail.com>
2025-11-23 10:35:35 +0300

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -14,3 +14,12 @@ urlpatterns = [
     path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),  # REQUIRED
     path("register/", views.register, name="register"),  # REQUIRED
 ]
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("admin-dashboard/", views.admin_view, name="admin_view"),
+    path("librarian-dashboard/", views.librarian_view, name="librarian_view"),
+    path("member-dashboard/", views.member_view, name="member_view"),
+]
