318b75eae0d402af12be9cf19139c70f6bce980b
Rabbadhaki <badha.ron09@gmail.com>
2025-11-23 10:34:00 +0300

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -72,3 +72,31 @@ def register(request):
             return redirect("list_books")
 
     return render(request, "relationship_app/register.html", {"form": form})
+
+from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
+from .models import UserProfile
+
+# Helper test functions
+def is_admin(user):
+    return hasattr(user, "userprofile") and user.userprofile.role == "Admin"
+
+def is_librarian(user):
+    return hasattr(user, "userprofile") and user.userprofile.role == "Librarian"
+
+def is_member(user):
+    return hasattr(user, "userprofile") and user.userprofile.role == "Member"
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, "relationship_app/admin_view.html")
+
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, "relationship_app/librarian_view.html")
+
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, "relationship_app/member_view.html")
