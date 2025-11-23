7f1d874d8a7f97badd5eb3243d2012815804849e
Rabbadhaki <badha.ron09@gmail.com>
2025-11-23 21:49:41 +0300

    Update urls.py

--- advanced_features_and_security/LibraryProject/bookshelf/urls.py
+++ advanced_features_and_security/LibraryProject/bookshelf/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("books/create/", views.create_book, name="create_book"),
     path("books/<int:pk>/edit/", views.edit_book, name="edit_book"),
     path("books/<int:pk>/delete/", views.delete_book, name="delete_book"),
+    path("books/", views.book_list, name="book_list"),
 ]
