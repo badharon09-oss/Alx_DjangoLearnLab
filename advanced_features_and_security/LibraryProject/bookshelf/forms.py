5010ee7cd2311ce1f96e88e65e9510e496fb2cc9
Rabbadhaki <badha.ron09@gmail.com>
2025-11-23 22:04:52 +0300

    Update forms.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -2,3 +2,7 @@ from django import forms
 
 class BookSearchForm(forms.Form):
     search = forms.CharField(required=False)
+class ExampleForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ["title", "author", "publication_year"]
