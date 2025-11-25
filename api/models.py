e1869ee6d037accae16c4e766f96e2b2a0123c85
badharon09-oss <badha.ron09@gmail.com>
2025-11-25 16:07:34 +0300

    Initial commit for advanced-api-project

---
+++ api/models.py
@@ -0,0 +1,30 @@
+# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+"""
+Author model:
+- Stores authors' names.
+- One author can have multiple books (One-to-Many relationship).
+"""
+
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+"""
+Book model:
+- Stores a book title and publication year.
+- Linked to Author using a ForeignKey (One Author → Many Books).
+"""
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    publication_year = models.IntegerField()
+    author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.title} ({self.publication_year})"
