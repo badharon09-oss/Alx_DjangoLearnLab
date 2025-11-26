8facde0ad1b42950122a01f645a662f6f97413ea
Rabbadhaki <badha.ron09@gmail.com>
2025-11-26 10:02:24 +0300

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -7,6 +7,9 @@ from .serializers import BookSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters import rest_framework
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
+
 
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
