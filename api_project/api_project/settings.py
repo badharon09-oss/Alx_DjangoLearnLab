b4b45a4b9847dfcf20c3b44d10ca005531dc7a09
Rabbadhaki <badha.ron09@gmail.com>
2025-12-01 09:45:02 +0300

    Update settings.py

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -129,3 +129,11 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',  # <-- REQUIRED
     ],
 }
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',  # <-- REQUIRED
+    ],
+}
