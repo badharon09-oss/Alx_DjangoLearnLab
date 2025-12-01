d2276336362c1ab72e9f2b2edcd9bb618afc5a35
Rabbadhaki <badha.ron09@gmail.com>
2025-12-01 09:16:19 +0300

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import serializers
+from .models import Book
+
+class BookSerializer(serializers.ModelSerializer):
+    """Serializer for the Book model including all fields."""
+
+    class Meta:
+        model = Book
+        fields = "__all__"
