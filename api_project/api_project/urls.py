4c0ee69094bbab6a438a8cddf18acb1da3d2b5e2
Rabbadhaki <badha.ron09@gmail.com>
2025-12-01 09:35:13 +0300

    Update urls.py

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -16,13 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
-from django.urls import path, include
-
-urlpatterns = [
+    # Token Authentication Endpoint
+    path('api/token/', obtain_auth_token, name='api-token'),
     path("api/", include("api.urls")),
 ]
+
+    
+
 
