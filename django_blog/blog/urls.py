f30c3c440df22b1b99f56375bab863179d0d2d9f
Rabbadhaki <badha.ron09@gmail.com>
2025-12-02 16:41:56 +0300

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -4,3 +4,19 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
 ]
+
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns = [
+    path("", views.home, name="home"),
+
+    # registration + profile
+    path("register/", views.register, name="register"),
+    path("profile/", views.profile, name="profile"),
+
+    # login/logout using Django built-ins
+    path("login/", auth_views.LoginView.as_view(template_name="blog/login.html"), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(template_name="blog/logout.html"), name="logout"),
+]
