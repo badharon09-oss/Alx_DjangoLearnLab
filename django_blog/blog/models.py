31b0bec3f3ca62002fe112dce505b55eeb16dbdb
Rabbadhaki <badha.ron09@gmail.com>
2025-12-03 17:19:19 +0300

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -19,3 +19,25 @@ class Meta:
 
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk': self.pk})
+from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+# existing Post model should be here
+
+class Comment(models.Model):
+    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['created_at']
+
+    def __str__(self):
+        return f'Comment by {self.author} on {self.post}'
+
+    def get_absolute_url(self):
+        # redirect back to the post detail page after actions
+        return reverse('post-detail', kwargs={'pk': self.post.pk})
