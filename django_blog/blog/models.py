cefd660f6ad8f1ce4ad41087e265bc7dba8e1210
Rabbadhaki <badha.ron09@gmail.com>
2025-12-03 18:43:23 +0300

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -48,3 +48,12 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
+
+from taggit.managers import TaggableManager
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    tags = TaggableManager()
