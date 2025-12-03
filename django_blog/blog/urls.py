0a95bfd7603ec49867896ea424997367c930ff90
Rabbadhaki <badha.ron09@gmail.com>
2025-12-03 17:20:50 +0300

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -40,3 +40,15 @@ urlpatterns = [
 
 
 path('', include('blog.urls')),   # already recommended earlier
+
+from . import views
+
+urlpatterns = [
+    # ... existing post urls ...
+    # comment creation (POST)
+    path('posts/<int:post_pk>/comments/new/', views.comment_create, name='comment-create'),
+
+    # comment edit & delete (by pk)
+    path('comments/<int:pk>/edit/', views.CommentUpdateView.as_view(), name='comment-update'),
+    path('comments/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment-delete'),
+]
