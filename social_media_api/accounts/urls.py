79bc7bf067080eb545490e3a84c683b9704e5034
Rabbadhaki <badha.ron09@gmail.com>
2025-12-17 23:43:59 +0300

    Update urls.py

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -6,3 +6,9 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('profile/', ProfileView.as_view(), name='profile'),
 ]
+from .views import follow_user, unfollow_user
+
+urlpatterns = [
+    path('follow/<int:user_id>/', follow_user, name='follow-user'),
+    path('unfollow/<int:user_id>/', unfollow_user, name='unfollow-user'),
+]
