532086255abe06efc589aa6a6165d6440317e87b
Rabbadhaki <badha.ron09@gmail.com>
2025-12-17 22:00:59 +0300

    Update settings.py

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -13,3 +13,12 @@ INSTALLED_APPS = [
     # Local
     'accounts',
 ]
+AUTH_USER_MODEL = 'accounts.User'
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
