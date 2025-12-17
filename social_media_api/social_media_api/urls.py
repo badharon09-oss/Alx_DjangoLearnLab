248c796368fed095a6bd515dda4a7cf70ccb0e7c
Rabbadhaki <badha.ron09@gmail.com>
2025-12-17 23:11:07 +0300

    Update urls.py

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -5,3 +5,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
 ]
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('accounts.urls')),
+    path('api/', include('posts.urls')),
+]
