70b17728dfd564f0f5a1d6ee8368e589f40bc276
Rabbadhaki <badha.ron09@gmail.com>
2025-12-17 23:45:13 +0300

    Update urls.py

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -6,3 +6,16 @@ router.register(r'posts', PostViewSet)
 router.register(r'comments', CommentViewSet)
 
 urlpatterns = router.urls
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+from .views import PostViewSet, CommentViewSet, FeedView
+
+router = DefaultRouter()
+router.register(r'posts', PostViewSet)
+router.register(r'comments', CommentViewSet)
+
+urlpatterns = [
+    path('feed/', FeedView.as_view(), name='user-feed'),
+]
+
+urlpatterns += router.urls
