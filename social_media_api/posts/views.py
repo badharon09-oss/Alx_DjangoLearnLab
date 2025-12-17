189efc3753537551f5164246907b78681f6f66d0
Rabbadhaki <badha.ron09@gmail.com>
2025-12-18 00:05:53 +0300

    Update views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -41,3 +41,24 @@ class FeedView(APIView):
 
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data)
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from .models import Post
+from .serializers import PostSerializer
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def feed(request):
+    """
+    Returns posts from users the current user follows,
+    ordered by most recent first.
+    """
+    user = request.user
+    following_users = user.following.all()
+
+    posts = Post.objects.filter(
+        author__in=following_users
+    ).order_by('-created_at')
+
+    serializer = PostSerializer(posts, many=True)
+    return Response(serializer.data)
