2923a19af95074fa8c2879cec9c91d7e950e4169
Rabbadhaki <badha.ron09@gmail.com>
2025-12-18 15:33:59 +0300

    Update views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -101,3 +101,22 @@ def unlike_post(request, pk):
     Like.objects.filter(user=user, post=post).delete()
 
     return Response({'status': 'post unliked'})
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def like_post(request, pk):
+    post = generics.get_object_or_404(Post, pk=pk)
+
+    like, created = Like.objects.get_or_create(
+        user=request.user,
+        post=post
+    )
+
+    if created:
+        Notification.objects.create(
+            recipient=post.author,
+            actor=request.user,
+            verb='liked your post',
+            target=post
+        )
+
+    return Response({'status': 'post liked'})
