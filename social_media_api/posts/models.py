2a979b2f2462d078db4d3f0cae8b8bb4cd78116b
Rabbadhaki <badha.ron09@gmail.com>
2025-12-18 14:54:51 +0300

    Update models.py

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -54,3 +54,19 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+from django.conf import settings
+from django.db import models
+
+User = settings.AUTH_USER_MODEL
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'post')
+
+    def __str__(self):
+        return f"{self.user} likes {self.post}"
